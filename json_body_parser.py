59359d3d56ccc671cb95a6fd426d4a9f7310d5a6
svenbaeck <85336555+svenbaeck@users.noreply.github.com>
2021-08-24 16:03:07 +0200

    Update json_body_parser.py

--- json_body_parser.py
+++ json_body_parser.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env python3
 
+"""
+Python 2 & 3 function to parse the JSON body of an AZURE Automation Runbook Webhook.
+"""
+
+import sys
+import json
+import re
+
+def parse_webhook_data():
+    request_body = ' '.join(sys.argv[1:])
+    regex = re.compile('{}(.*){}'.format(re.escape('RequestBody:'), re.escape(',RequestHeader:')))
+    clean_body = regex.findall(request_body)
+    return json.loads(clean_body[0])
