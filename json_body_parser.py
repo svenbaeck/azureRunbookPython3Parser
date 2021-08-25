4e4056a95beb801f62aa5186fac0828a3e270453
svenbaeck <85336555+svenbaeck@users.noreply.github.com>
2021-08-25 13:47:35 +0200

    Update json_body_parser.py
    Added support for managed identity authentication

--- json_body_parser.py
+++ json_body_parser.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 
 """
-Python 2 & 3 function to parse the JSON body of an AZURE Automation Runbook Webhook.
+Python 2 & 3 function to:
+  - authenticate using a managed identity assigned to the automation account
+  - parse the JSON body of an AZURE Automation Runbook Webhook.
 """
 
 import sys
 import json
 import re
+from azure.identity import DefaultAzureCredential
 
 def parse_webhook_data():
     request_body = ' '.join(sys.argv[1:])
     regex = re.compile('{}(.*){}'.format(re.escape('RequestBody:'), re.escape(',RequestHeader:')))
     clean_body = regex.findall(request_body)
     return json.loads(clean_body[0])
+
+def get_managed_identity_credential():
+    return DefaultAzureCredential()
